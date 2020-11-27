b778de47bf25c99703cd79f6025b131db85ccf0e
JuanDaniel2510 <juan_daniel_2510@hotmail.com>
2020-11-27 21:46:18 +0000

    improved test 2

--- test2.js
+++ test2.js
@@ -5,36 +5,46 @@ const fs = require('fs');
 const URLS = [
     "https://www.pccomponentes.com/msi-mpg-x570-gaming-pro-carbon-wifi",
     "https://www.pccomponentes.com/kingston-hyperx-fury-black-ddr4-3600-16gb-2x8gb-cl17",
+    "https://www.pccomponentes.com/kingston-hyperx-fury-rgb-ddr4-3600mhz-pc4-28800-16gb-cl17",
+    "https://www.pccomponentes.com/kingston-hyperx-fury-rgb-ddr4-3600mhz-pc4-28800-8gb-cl17",
+    "https://www.pccomponentes.com/crucial-ballistix-ddr4-3600mhz-pc4-28800-16gb-2x8gb-cl16",
     "https://www.pccomponentes.com/amd-ryzen-9-5950x-34-ghz"
 ];
 
-async function runTest(mode, url) {
-    console.log("Loadding browser...");
-    const browser = await puppeteer.launch({
-        args: ['--no-sandbox'],
-        headless: mode === 'headless',
-    });
-    const page = await browser.newPage();
+async function runTest(page, url) {
     console.log("Fetching page...");
     await page.goto(url);
 
     console.log("Getting item data...");
-    console.log("ITEM DATA:", await page.evaluate(() => {
-        return {
+    let itemData = await page.evaluate(() => {
+        let data = {
             name: document.getElementsByClassName("articulo")[0].innerText,
+            serial: document.querySelector(".ficha-producto__datos-de-compra > div:nth-child(2) > div:nth-child(2) > span:nth-child(2)").innerText,
             shop: document.getElementsByClassName("ficha-producto__encabezado")[0].children[1].lastChild.children[0].innerText.trim(),
-            price: Number(document.getElementsByClassName("no-iva-base")[0].innerText.replaceAll(',', '.')),
+            price: Number(Math.floor(document.getElementsByClassName("no-iva-base")[0].innerText.replaceAll(',', '.')*107)/100),
             avaliable: document.getElementById("notify-me") == undefined
         }
-    }));
-
-
-    console.log("Closing browser...");
-    browser.close();
+        data.purchasable = (data.avaliable && data.shop == 'PcComponentes');
+        return data;
+    });
+    console.log("Done.");
+    return itemData;
 }
 
 (async () => {
+    console.log("Loadding browser...");
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        headless: true,
+    });
+    const page = await browser.newPage();
+    let data = [];
     for (url of URLS) {
-        await runTest('headless', url);
+        data.push(await runTest(page, url));
     }
+    console.log("Closing browser...");
+    browser.close();
+    
+    console.log("\nData:")
+    console.log(data);
 })();
